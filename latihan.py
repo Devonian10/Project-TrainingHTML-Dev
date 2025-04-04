a5273646cc10c2a426c18773a99633b9397ad61a
Devonian P10 <54732787+Devonian10@users.noreply.github.com>
2025-04-04 15:26:17 +0800

    Update Training Programming

--- latihan.py
+++ latihan.py
@@ -42,3 +42,41 @@ name3 = "Devonian"
 
 print(f'{name3}')
 
+money = int(input("Silahkan isi uangmu : "))
+food = ['Nasi Goreng', 'Ayam Kampung', 'Ayam Goreng Lalapan', 'Capcay', 'Ikan Goreng Tepung', 'Cumi-cumi Goreng', 'Cumi-cumi Hitam', 'Ikan Bolu', 'Ikan Bakar']
+price_food=(16000, 20000, 22000,20000,25000,30000, )
+drink = ["Jus Alpukat", 'Jus Jeruk', 'Jus Apel', 'Jus Kelp']
+price_drink = (15000, 7000, 10000, 30000)
+res1 = []
+menu = input("Silahkan pilih menu (food/drink) : ").strip()
+#Ini untuk makanan
+menu1 = input('Pilih Pemesanan anda : ').strip()
+if (menu == 'food'): 
+    if (menu1 == 'Nasi Goreng'):
+        money -= price_food[0]
+        res1 = money
+    elif (menu1 == 'Ayam Kampung'):
+        money -= price_food[1]
+        res1 = money
+
+    elif (menu1 == 'Ayam Goreng Lalapan'):
+        money -=price_food[2]
+        res1 = money
+
+    elif (menu1 == 'Capcay'):
+        money -= price_food[3]
+        res1 = money
+#Ini Untuk Minuman
+elif (menu == 'drink'):
+    if (menu1 == 'Jus Alpukat'):
+        money -= price_drink[0]
+        res1 = money
+    elif (menu1 == 'Jus Apel'):
+        money -= price_drink[1]
+        res1 = money
+    elif (menu1 == 'Jus Kelp'):
+        money -= price_drink[2]
+        res1 = money
+else:
+    print('Tidak ada kategori ')
+print(f'Kembalian uang anda adalah {res1}')
--- latihan2.py
+++ latihan2.py
@@ -12,7 +12,7 @@ spd = int(input("Tulis SPD kamu : "))
 
 print(f"Daftar kateogri sebagai berikut:")
 print(f"")
-choose = input("Pilih Kategori senjata: ")
+choose = input("Pilih Kategori senjata (Polearm/Sword): ").strip()
 if (choose == 'Polearm'):
     polearm = atk =- 100
     res=print("LOL")
@@ -20,7 +20,23 @@ elif(choose == 'Sword'):
     sword = atk =- 100
     res=sword
 
-monster = input("Pilih Kategorti monster")
-if (monster == 'Slime'):
-    slime = 1000 =- atk =- em
-print(f'{res}')
+monster = input("Pilih kategori monster (Slime/Hillicurl): ").strip()
+def calculate_damage(monster, atk, em):
+    if (monster == 'Slime'):
+        slime = 1000 - (atk - em)  # HP Slime dikurangi ATK dan ditambah EM
+        res1 = slime
+    elif (monster == 'Hillicurl'):
+        if (em != 0):
+            hillicurl = 1000 - (atk // em)  # Pembagian integer jika EM tidak nol
+        else:
+            hillicurl = 1000 - atk  # Jika EM nol, hanya dikurangi ATK
+        res1 = hillicurl
+    else: 
+        res1 = 1000  # Jika monster tidak dikenali, HP tetap 1000
+    return res1  
+hp_sisa = calculate_damage(monster, atk, em)
+print(f"HP {monster} setelah terkena serangan: {hp_sisa}")
+print(f'hasil atk adalah {res}', f' dan monster adalah {monster}')
+
+
+
