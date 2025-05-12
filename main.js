e35e53e92f7c785465ded0fd759b8f4be231faad
Devonian P10 <54732787+Devonian10@users.noreply.github.com>
2025-05-12 22:24:11 +0800

    Update Characters and Descriptions

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  document.getElementById("Devon").innerHTML = "Hello Javascript";
+  document.getElementsByClassName("Devon")[0].style.color = `#102E50`;
+  document.getElementById("Devon").style.background= "#F5C45E";
+}
+
+function LOL(){
+    document.getElementsByTagName("LOLMU").namedItem = "Ay Siap";
+
+}
